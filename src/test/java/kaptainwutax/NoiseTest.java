54481cb294c5b6674d70dda326a20883765d5589
Neil <h66r10@gmail.com>
2021-03-07 17:53:08 +0100

    fix( dependencies): add test

---
+++ src/test/java/kaptainwutax/NoiseTest.java
@@ -0,0 +1,20 @@
+package kaptainwutax;
+
+import kaptainwutax.noiseutils.perlin.OctavePerlinNoiseSampler;
+import kaptainwutax.seedutils.mc.ChunkRand;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NoiseTest {
+
+
+    @Test
+    @DisplayName("Test Perlin noise")
+    public void testHeight() {
+        ChunkRand rand=new ChunkRand();
+        OctavePerlinNoiseSampler perlinNoiseSampler = new OctavePerlinNoiseSampler(rand, 16);
+        assertEquals(perlinNoiseSampler.sample(0,0,0),-1830.633256896282);
+    }
+}
